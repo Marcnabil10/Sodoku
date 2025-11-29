e774c0af4e8d70021685126c4fd0397380b5ae6e
antoniosabdelmassih-prog <antoniosabdelmasieh@gmail.com>
2025-11-29 23:35:15 +0200

    tony

--- src/BackEnd/ResultFormatter.java
+++ src/BackEnd/ResultFormatter.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package BackEnd;
 
 import java.util.Collections;
 import java.util.List;
 
-/**
- *
- * @author Ʈ Ꝺ Ɲ ƴ
- */
 public class ResultFormatter {
 
     public void formatAllFailures(List<FailureDetail> failures) {
@@ -25,14 +17,24 @@ public class ResultFormatter {
 
     private String formatSingleFailure(FailureDetail failure) {
         StringBuilder sb = new StringBuilder();
+        
         sb.append(failure.getType().toUpperCase()).append(" ");
         sb.append(failure.getIndex() + 1);
+        
         sb.append(", #").append(failure.getDuplicatedValue());
-        sb.append(", ");
+        
+        sb.append(", [");
         List<Integer> positions = failure.getPositions();
         Collections.sort(positions);
-        sb.append(positions.toString());
+        
+        for (int i = 0; i < positions.size(); i++) {
+            sb.append(positions.get(i) + 1);  
+            if (i < positions.size() - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        
         return sb.toString();
-
     }
 }
