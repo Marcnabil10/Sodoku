8044c430672894b08e2289b72e6b8c09c518b4bd
antoniosabdelmassih-prog <antoniosabdelmasieh@gmail.com>
2025-11-29 17:01:29 +0200

    tony

--- src/BackEnd/InputParser.java
+++ src/BackEnd/InputParser.java
@@ -13,39 +13,39 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
 public class InputParser {
-    public int[][] loodCsv (String filePath)
-    {
-        int [][] board = new int [9][9];
-        BufferedReader br=null;
-        
-        try
-        {
-            br = new BufferedReader( new FileReader(filePath));
+
+    public int[][] loadCsv(String filePath) {
+        int[][] board = new int[9][9];
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(filePath));
             String line;
-            int rowIndex =0;
-            while((line = br.readLine()) != null && rowIndex<9)
-                    {
-                        String[] values = line.split(",");
-                        for(int colIndex = 0;colIndex< values.length&&colIndex<9; colIndex++){
-                            board[rowIndex][colIndex]=Integer.parseInt(values[colIndex].trim());
-                        }
-                        rowIndex++;
-                    }
-            
-        }catch (IOException e)
-        {
+            int rowIndex = 0;
+            while ((line = br.readLine()) != null && rowIndex < 9) {
+                String[] values = line.split(",");
+                for (int colIndex = 0; colIndex < values.length && colIndex < 9; colIndex++) {
+                    board[rowIndex][colIndex] = Integer.parseInt(values[colIndex].trim());
+                }
+                rowIndex++;
+            }
+            if (rowIndex<9)
+            {
+                System.err.println("Warning CSV has only " + rowIndex +"rows . padding with zeros.");
+            }
+
+        } catch (IOException e) {
             throw new RuntimeException("Error reading the file :" + filePath, e);
-        }
-        catch (NumberFormatException e)
-        {
-            throw new RuntimeException("Error parsing number from CSV. ",e);
-        }finally
-        {
-            if(br!=null)
-                try {br.close();}
-            catch(IOException e){
-                System.err.println("Error closing file : "+e.getMessage());
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Error parsing number from CSV. ", e);
+        } finally {
+            if (br != null)
+                try {
+                br.close();
+            } catch (IOException e) {
+                System.err.println("Error closing file : " + e.getMessage());
             }
         }
         return board;
